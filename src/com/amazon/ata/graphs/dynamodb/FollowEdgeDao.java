06cc13076d7cd8a458d636cd201558a1b3945c08
Ryan Krauss <rlkrauss02@gmail.com>
2025-05-31 11:53:57 -0400

    sprint 28 done

--- src/com/amazon/ata/graphs/dynamodb/FollowEdgeDao.java
+++ src/com/amazon/ata/graphs/dynamodb/FollowEdgeDao.java
@@ -2,7 +2,6 @@ package com.amazon.ata.graphs.dynamodb;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
-import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedList;
 import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedQueryList;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
@@ -10,45 +9,14 @@ import javax.inject.Inject;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Provides access to FollowEdge items.
- */
 public class FollowEdgeDao {
     private DynamoDBMapper mapper;
 
-    /**
-     * Creates a FollowEdgeDao with the given DynamoDBMapper.
-     * @param mapper The DynamoDBMapper
-     */
     @Inject
     public FollowEdgeDao(DynamoDBMapper mapper) {
         this.mapper = mapper;
     }
 
-    /**
-     * Retrieves a list of follows from the given username, if one exists.
-     * @param username The username to look for
-     * @return A list of all follows for the given user
-     */
-    public PaginatedQueryList<FollowEdge> getAllFollows(String username) {
-        return null;
-    }
-
-    /**
-     * Retrieves a list of followers for the given username, if one exists.
-     * @param username The username to scope followers to
-     * @return A list of all followers for the given user
-     */
-    public PaginatedQueryList<FollowEdge> getAllFollowers(String username) {
-        return null;
-    }
-
-    /**
-     * Saves new follow.
-     * @param fromUsername The Member that is following
-     * @param toUsername The Member that is followed
-     * @return The FollowEdge that was created
-     */
     public FollowEdge createFollowEdge(String fromUsername, String toUsername) {
         if (null == fromUsername || null == toUsername) {
             throw new IllegalArgumentException("One of the passed in usernames was null: " + fromUsername + " was trying to follow " + toUsername);
@@ -59,4 +27,27 @@ public class FollowEdgeDao {
         return edge;
     }
 
+    public PaginatedQueryList<FollowEdge> getAllFollows(String username) {
+        if (username == null || username.isEmpty()) {
+            throw new IllegalArgumentException("username not provided");
+        }
+        DynamoDBQueryExpression<FollowEdge> queryExpression = new DynamoDBQueryExpression<>();
+        FollowEdge parameter = new FollowEdge(username, null);
+        queryExpression.withHashKeyValues(parameter);
+        return this.mapper.query(FollowEdge.class, queryExpression);
+    }
+
+    public PaginatedQueryList<FollowEdge> getAllFollowers(String username) {
+        if (username == null || username.isEmpty()) {
+            throw new IllegalArgumentException("username not provided");
+        }
+        DynamoDBQueryExpression<FollowEdge> queryExpression = new DynamoDBQueryExpression<>();
+        Map<String, AttributeValue> attributeValueMap = new HashMap<>();
+        attributeValueMap.put(":toUsername", new AttributeValue().withS(username));
+
+        queryExpression.withKeyConditionExpression("toUsername = :toUsername")
+                .withExpressionAttributeValues(attributeValueMap);
+
+        return this.mapper.query(FollowEdge.class, queryExpression);
+    }
 }
